6b72161420591bcc98b45a181ef7f377b2a37635
iOmega17 <77457176+iOmega17@users.noreply.github.com>
2025-04-28 01:55:29 +0530

    Initial commit

--- reactapp/src/apiConfig.js
+++ reactapp/src/apiConfig.js
@@ -1,3 +1,3 @@
 
-const API_BASE_URL = 'https://8080-bfdfdcabdadfebcbebfdaeaeddcedeabebdb.premiumproject.examly.io/api';
+const API_BASE_URL = 'http://localhost:8080/api';
 export default API_BASE_URL;
